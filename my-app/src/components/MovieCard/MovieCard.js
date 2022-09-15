c962cd79988511e87c9329f95ec1975809ef77c1
franciscojofre <jjofrelopez@udesa.edu.ar>
2022-09-15 11:28:43 -0300

    nueva logica add favs y ver mas/menos con MovieCard statefull

--- my-app/src/components/MovieCard/MovieCard.js
+++ my-app/src/components/MovieCard/MovieCard.js
@@ -1,32 +1,102 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import './MovieCard.css'
 import { Link } from 'react-router-dom'
 
-function MovieCard (props){
-    
-const [isActive, setIsActive] = useState(false)
+class MovieCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            favText: 'Agregar a favoritos',
+            verDesc: false
+        }
+    };
 
-const handleClick = () => {
- //   setIsActive(current => !current)
-}
+    mostrar(){
+        this.setState( {verDesc: true} )
+    }
+    esconder(){
+        this.setState({verDesc: false} )
+    }
+
+    componentDidMount(){
+        let favoritos = [];
+        let recuperoStorage = localStorage.getItem('favoritos');
+
+        if (recuperoStorage !== null) {
+            let storageToArray = JSON.parse(recuperoStorage);
+            favoritos = storageToArray
+
+            if (favoritos.includes(this.props.id)) {
+                this.setState({
+                    favText: 'Quitar de favoritos'
+                })
+            } else {
+                this.setState({
+                    favText: 'Agregar a favoritos'
+                })
+            }
+        }
+    }
+
+    agregarQuitarFavs(id){
+        // console.log('agregando y quitando');
+
+        let favoritos = [];
+        let recuperoStorage = localStorage.getItem('favoritos');
+
+        if (recuperoStorage !== null) {
+            let storageToArray = JSON.parse(recuperoStorage);
+            favoritos = storageToArray
+        }
+
+        if (favoritos.includes(this.props.id)) {
+            // favoritos = favoritos.filter(unIdDelArray => unIdDelArray !== id) //FORMA 1//
+            let idAQuitar = favoritos.indexOf(this.props.id);
+            favoritos.splice(idAQuitar, 1);
+            this.setState({
+                favText: 'Agregar a favoritos'
+            })
+        } else {
+            favoritos.push(this.props.id);
+            this.setState({
+                favText: 'Quitar de favoritos'
+            })
+        }
+
+        let favsToString = JSON.stringify(favoritos)
+        localStorage.setItem('favoritos', favsToString)
+
+        console.log(localStorage);
+    }
+
+    render(){
     return ( 
             <ul id="film">
-                <Link to={`/movie/detail/${props.id}`}>
-                    <li className="movieTitle"><h3>{props.title}</h3></li>
-                    {props.img.includes(null) ? 
+                <Link to={`/movie/detail/${this.props.id}`}>
+                    <li className="movieTitle"><h3>{this.props.title}</h3></li>
+                    {this.props.img.includes(null) ? 
                         <li><div className="styleGiphy"><iframe src="https://giphy.com/embed/21xxijoZGAS4zPpDT9" frameBorder="0" class="giphy-embed" title="giphNoMovie"></iframe></div></li>
                     :
-                        <li className="movieImg"><img alt="imgFilm" src={props.img}/></li>
+                        <li className="movieImg"><img alt="imgFilm" src={this.props.img}/></li>
                     }
                     {/*<li><img alt="imgFilm" src={props.img !== null ? props.img : props}/></li>*/}             
-                    <li className="filmDescription" style={{display: isActive ? 'block' : 'none'}}>{props.info}</li>
-                    <div className="buttonscards">
-                    <li onClick={handleClick}><button onClick={() => setIsActive(!isActive)} className='buttonscards'> {isActive ? 'Hide Extra' : 'Extra'}   </button> 
-                    <button className="buttonscards">Add Favorites</button></li> </div>
                 </Link>
+                    <div className="buttonscards">
+                        {this.state.verDesc ? 
+                        <section>                            
+                            <li className="filmDescription">Overview: {this.props.info}</li> 
+                            <button className='more' onClick={() => this.esconder()}>Ver menos</button>
+                        </section>
+                        :                                                 
+                        <button className='more' onClick={() => this.mostrar()}>Ver mas</button>                         
+                        
+                    } 
+                    <button onClick={() => this.agregarQuitarFavs(this.props.id)} className="buttonscards">{this.state.favText}</button>
+                    </div>
                 
             </ul>
     )
+    }
 }
 
 export default MovieCard;
